6ffaf55755659cb27e6950d66f378da363e68646
nf02 <ask.nfvblog@outlook.it>
2023-06-10 14:44:54 +0200

    manuale octave

---
+++ Rudimenti/GNUOctave/source/moltM2x3.m
@@ -0,0 +1,9 @@
+%% Prima operazione
+A = [ 2, 0; 3, -1]; % Crea la prima matrice 
+B= [ 4, -1; 1, 2]; % Crea la seconda matrice
+ris = 2*A-3*B; % svolge la prima operazione (2A-3B).
+ris % stampa il risultato
+
+%% seconda operazione
+ris =3*A-2*B;
+ris
